7aa850f6fdda2e5e73feb9c21b3de27bb89ab6b6
Laika <lgair@sfu.ca>
2020-08-11 19:46:37 -0700

    fixed segfault error

--- main.cpp
+++ main.cpp
@@ -123,6 +123,7 @@ int main(int argc, char const *argv[])
     cout << endl;
     rootNode->spawnChildren();
     rootNode->printChildren();
+    ChildNode->spawnChildren();
     ChildNode->nodeExpansion();
 
     return 0;
